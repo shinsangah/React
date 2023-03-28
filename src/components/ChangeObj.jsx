14f6dda36f566b8aeab6add8867dce6a6d9c59e6
sang ah <kr.sangah@gmail.com>
2023-03-28 22:47:51 +0900

    230328

--- src/components/ChangeObj.jsx
+++ src/components/ChangeObj.jsx
@@ -17,7 +17,7 @@ export default function ChangeObj(props) {
       <h1>이름 : {obj.name}</h1>
       <h2>나이 : {obj.age}</h2>
       <h2>별명 : {obj.nickName}</h2>
-      <button onClick={() => { index === props.objArr.length - 1 ? setIndex((cur) => 0) : setIndex((cur) => cur +1) }}>프로필 변경하기</button>
+      <button onClick={() => { index === props.objArr.length - 1 ? setIndex((cur) => 0) : setIndex((cur) => cur + 1) }}>프로필 변경하기</button>
     </div>
   )
 }
