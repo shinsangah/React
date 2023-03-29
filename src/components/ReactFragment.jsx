53a5a929e176f10e7312c9fcd908755c88af2a1f
sang ah <kr.sangah@gmail.com>
2023-03-29 15:37:17 +0900

    230329

---
+++ src/components/ReactFragment.jsx
@@ -0,0 +1,12 @@
+// ReactFragment : 축약 (fragment: 파편)
+
+export default function ReactFragment() {
+  return (
+    <>
+      <h1>안녕하세요!</h1>
+      <span>반갑습니다!</span>
+    </>
+  );
+}
+
+// CSS 구축을 해놨는데 div 요소로 인해 꺠질 때 해방
