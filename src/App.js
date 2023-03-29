b63322f832fe7e7f74afd5bc0bf65cea71111896
sang ah <kr.sangah@gmail.com>
2023-03-29 11:48:46 +0900

    230329

--- src/App.js
+++ src/App.js
@@ -1,42 +1,39 @@
-import "./App.css";
-import MainHeader from "./components/MainHeader";
-import ImgComponent from "./components/ImgComponent";
-import BtnToNaver from "./components/BtnToNaver";
-import ClassComponent from "./components/ClassComponent";
-import FunctionalComponent from "./components/FunctionalComponent";
-import Inline from "./components/Inline";
-import EventHandler from "./components/EventHandler";
-import Example1 from "./components/Example1";
-import State from "./components/State";
-import State2 from "./components/State2";
-import Counter from "./components/Conter";
-import Condition from "./components/Condition";
-import Emoji from "./components/Emoji";
-import ClassState from "./components/ClassState";
-import StateAndVariable from "./components/StateAndVariable";
-import StateProblem from "./components/StateProblem";
-import List from "./components/List";
-import Modal from "./components/Modal";
-import PropsHeader from "./components/PropsHeader";
-import MultiProps from "./components/MultiProps";
-import ClassProps from "./components/ClassProps";
-import ListChild from "./components/ListChild";
-import ItemList from "./components/ItemList";
-import CustomList from "./components/CustomList";
-import CustomObj from "./components/CustomObj";
-import ExampleChangeObj from "./components/ExampleChangeObj";
+import './App.css';
+import MainHeader from './components/MainHeader';
+import ImgComponent from './components/ImgComponent';
+import BtnToNaver from './components/BtnToNaver';
+import ClassComponent from './components/ClassComponent';
+import FunctionalComponent from './components/FunctionalComponent';
+import Inline from './components/Inline';
+import EventHandler from './components/EventHandler';
+import Example1 from './components/Example1';
+import State from './components/State';
+import State2 from './components/State2';
+import Counter from './components/Conter';
+import Condition from './components/Condition';
+import Emoji from './components/Emoji';
+import ClassState from './components/ClassState';
+import StateAndVariable from './components/StateAndVariable';
+import StateProblem from './components/StateProblem';
+import List from './components/List';
+import Modal from './components/Modal';
+import PropsHeader from './components/PropsHeader';
+import MultiProps from './components/MultiProps';
+import ClassProps from './components/ClassProps';
+import ListChild from './components/ListChild';
+import ItemList from './components/ItemList';
+import CustomList from './components/CustomList';
+import CustomObj from './components/CustomObj';
+import ExampleChangeObj from './components/ExampleChangeObj';
+import TestRef from './components/TestRef';
+import ChangeFocus from './components/ChangeFocus';
+import RefDomControl from './components/RefDomControl';
+import ColorEx from './components/ColorEx';
 
 function App() {
-  const nameArr = ["뽀로로", "루피", "크롱이"];
-  const pororoObj = {
-    name: "뽀로로",
-    age: "5",
-    nickName: "사고뭉치",
-  };
-
   return (
     <div className="App">
-      <ExampleChangeObj />
+      <ColorEx />
     </div>
   );
 }
--- src/components/ChangeObj.jsx
+++ src/components/ChangeObj.jsx
@@ -11,13 +11,21 @@ export default function ChangeObj(props) {
     } else {
       setIndex(index + 1);
     }
-  }
+  };
   return (
     <div>
       <h1>이름 : {obj.name}</h1>
       <h2>나이 : {obj.age}</h2>
       <h2>별명 : {obj.nickName}</h2>
-      <button onClick={() => { index === props.objArr.length - 1 ? setIndex((cur) => 0) : setIndex((cur) => cur + 1) }}>프로필 변경하기</button>
+      <button
+        onClick={() => {
+          index === props.objArr.length - 1
+            ? setIndex((cur) => 0)
+            : setIndex((cur) => cur + 1);
+        }}
+      >
+        프로필 변경하기
+      </button>
     </div>
-  )
+  );
 }
