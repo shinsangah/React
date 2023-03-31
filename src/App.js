7ca0e2a6fa358d989353eae60eb7d113297c4248
sang ah <kr.sangah@gmail.com>
2023-03-31 11:48:13 +0900

    230331

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import './App.css';
-import TestStyled from './components/TestStyled';
+// import './App.css';
+import ExampleStyled from './components/ExampleStyled';
 
 function App() {
   return (
     <div className="App">
-      <TestStyled />
+      <ExampleStyled />
     </div>
   );
 }
