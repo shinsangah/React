53921ade9c8146a54b37645b8450a1d033cded15
sang ah <kr.sangah@gmail.com>
2023-04-03 11:07:53 +0900

    230403

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
-import ExampleStyled from './components/ExampleStyled';
-import PropsStyle from './components/PropsStyle';
+import FancyBorder from './components/FancyBorder';
+import ImgComponent2 from './components/ImgComponent2';
 
 function App() {
   return (
     <div className="App">
-      <PropsStyle color="orange" />
+      <FancyBorder color="blue">
+        <h1>Hello, props.children</h1>
+        <p>이건 매우 유용한 기술입니다!</p>
+        {/* props.children */}
+        <ImgComponent2 />
+      </FancyBorder>
     </div>
   );
 }
